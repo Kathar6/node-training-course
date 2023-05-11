0fc1d57aa49ee9e55823953682e9a3b05c903b6d
Mateo Pabon Garcia <matepabon@gmail.com>
2023-05-11 18:59:32 -0500

    Start using mongodb to create a user schema

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-console.clear();
-import dotenv from "dotenv";
-dotenv.config();
-import { start } from "./src/server.js";
+console.clear()
+import dotenv from "dotenv"
+dotenv.config()
+import { bootstrap } from "./src/server.js"
 
-const PORT = process.env.PORT || process.argv[2] || 3000;
-start(PORT);
+const PORT = process.env.PORT || process.argv[2] || 3000
+bootstrap(PORT)
--- src/server.js
+++ src/server.js
@@ -1,28 +1,37 @@
-import express from "express";
-import cookieParser from "cookie-parser";
+import express from "express"
+import cookieParser from "cookie-parser"
+import mongoose from "mongoose"
 
 //#region routes
-import usersRouter from "./routes/users.js";
-import authRouter from "./routes/auth.js";
-import authTokenRouter from "./routes/auth-token.js";
-import authSessionRouter from "./routes/auth-session.js";
+import usersRouter from "./routes/users.js"
+import authRouter from "./routes/auth.js"
+import authTokenRouter from "./routes/auth-token.js"
+import authSessionRouter from "./routes/auth-session.js"
 //#endregion
 
-const app = express();
+const app = express()
 
-app.use(express.json()); // Format JSON payloads
-app.use(express.text()); // Format Plain text payloads
-app.use(cookieParser()); // Format cookies
+app.use(express.json()) // Format JSON payloads
+app.use(express.text()) // Format Plain text payloads
+app.use(cookieParser()) // Format cookies
 //#region routes registration
-app.use("/auth", authRouter);
-app.use("/account", usersRouter);
-app.use("/token", authTokenRouter);
-app.use("/session", authSessionRouter);
+app.use("/auth", authRouter)
+app.use("/account", usersRouter)
+app.use("/token", authTokenRouter)
+app.use("/session", authSessionRouter)
 //#endregion
 
-export const start = (port) => {
-  if (!port) return false;
+/**
+ * Connect to MongoDB server, then, start the express server
+ * @param {number} port Server port to listen
+ * @returns
+ */
+export const bootstrap = async (port) => {
+  if (!port) return false
+
+  await mongoose.connect(process.env.MONGODB_URL)
+
   app.listen(port, () => {
-    console.log(`Server listening in port: ${port}`);
-  });
-};
+    console.log(`Server listening in port: ${port}`)
+  })
+}
